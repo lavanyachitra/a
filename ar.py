f1b72fc0048f75de7d83262d50631b1d3c23a7fa
lavanyachitra <lavanya19399@gmail.com>
2019-06-07 16:25:54 +0530

    Update ar.py

--- ar.py
+++ ar.py
@@ -8,7 +8,7 @@ if m==x:
 else:
     for i in range(0, f1 - 2):
         for j in range(i + 1, f1 - 1):
-            for k in range(j + 1, q1):
+            for k in range(j + 1, f1):
                 if m[i] < m[j] < m[k] and i < j < k:
                     s = s + 1
     print(s)
