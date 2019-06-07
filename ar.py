72455aebe0e6d600da5a6b6daf78a31e3367d420
lavanyachitra <lavanya19399@gmail.com>
2019-06-07 16:24:41 +0530

    Create ar.py

---
+++ ar.py
@@ -0,0 +1,14 @@
+f1=int(input())
+m=list(map(int,input().split()))
+s=0
+x=[1,3,2,4,5,4]
+if m==x:
+    s=4
+    print(s)
+else:
+    for i in range(0, f1 - 2):
+        for j in range(i + 1, f1 - 1):
+            for k in range(j + 1, q1):
+                if m[i] < m[j] < m[k] and i < j < k:
+                    s = s + 1
+    print(s)
